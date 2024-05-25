ab3b7515e8589565f1dae4bbaecfa91c1df2977c
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2024-05-25 16:45:06 +0900

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,17 @@
+def solution(A):
+    # Implement your solution here
+    dict_found = {}
+    leng_array = len(A)
+
+    for i in range(leng_array-1, -1, -1):
+        if A[i] in dict_found.keys():
+            dict_found[A[i]] += 1
+        else:
+            dict_found[A[i]] = 1
+    
+    for key, value in dict_found.items():
+        if value % 2 == 1:
+            return key
+    return None
+
+solution([9, 3, 9, 3, 9, 7, 9]) # 7
